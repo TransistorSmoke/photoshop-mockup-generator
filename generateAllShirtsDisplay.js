e108e57d0dbbc1f46250219b8f53a6db50a473e4
Jason <jmaalegado@gmail.com>
2024-07-02 21:58:59 +1000

    Crop and translate each shirt mock to upper left of canvas - no arrangement yet

--- generateAllShirtsDisplay.js
+++ generateAllShirtsDisplay.js
@@ -15,13 +15,17 @@ var BASEPATH = scriptFile.parent;
 var EXPORTPATH = BASEPATH + "/exports";
 
 var MOCKUPS = "MOCKUPS";
+var RESIZED = "RESIZED";
 var PCT = 0.6;  // Resize percentage
 
 var canvasWidth = doc.width;
 var canvasHeight = doc.height;
-var allMockupsGroups = doc.layerSets.getByName(MOCKUPS);
+var allMockupsGroup = doc.layerSets.getByName(MOCKUPS);
+var resizedGroup = doc.layerSets.getByName(RESIZED);
 
-var singleMockup = allMockupsGroups.layers[0];
+// alert("inside resied group: " + resizedGroup.layers[0].name )
+
+var singleMockup = allMockupsGroup.layers[0];
 var singleMockupWidth = singleMockup.bounds[2] - singleMockup.bounds[0];
 var singleMockupHeight = singleMockup.bounds[3] - singleMockup.bounds[1];
 
@@ -44,54 +48,65 @@ var singleMockupHeight = singleMockup.bounds[3] - singleMockup.bounds[1];
 
 
 
-// CROPPING Procedure
 try {
-
-  // Test - crop first then translate
   var left = canvasWidth/2 - singleMockupWidth/2 + 250;
   var top = canvasHeight/2 - singleMockupHeight/2;
   var right = canvasWidth/2 + singleMockupWidth/2 - 250;
   var bottom = canvasHeight - singleMockupHeight/2;
   var cropBounds = [left, top, right, bottom];
-
-
-  var activeMockLayer = singleMockup;
-  // var duplicateActiveMockLayer = activeMockLayer.duplicate();
-  // duplicateActiveMockLayer.name = activeMockLayer.name + " (cropped)";
-  // activeMockLayer.name = activeMockLayer.name + " (cropped)";
-
+  // var activeMockLayer = singleMockup;
+  var newCroppedLayerWidth = 0;
+  var newCroppedLayerHeight = 0;
 
   // Select the layer
-  doc.selection.select([
-    [cropBounds[0], cropBounds[1]],
-    [cropBounds[2], cropBounds[1]],
-    [cropBounds[2], cropBounds[3]],
-    [cropBounds[0], cropBounds[3]],
-  ])
+  // doc.selection.select([
+  //   [cropBounds[0], cropBounds[1]],
+  //   [cropBounds[2], cropBounds[1]],
+  //   [cropBounds[2], cropBounds[3]],
+  //   [cropBounds[0], cropBounds[3]],
+  // ])
 
-  doc.selection.copy();
-  var newCroppedLayer = allMockupsGroups.artLayers.add();
-  newCroppedLayer.name = activeMockLayer.name + "- cropped";
-  doc.paste();
-  doc.selection.deselect();
-  var newCroppedLayerBounds = newCroppedLayer.bounds;
-  activeMockLayer.visible = false;
+  // doc.selection.copy();
+  // var newCroppedLayer = allMockupsGroups.artLayers.add();
+  // newCroppedLayer.name = activeMockLayer.name + "- cropped";
+  // doc.paste();
+  // doc.selection.deselect();
+  // var newCroppedLayerBounds = newCroppedLayer.bounds;
+  // activeMockLayer.visible = false;
 
-  // Resize here
-  // ----------------
-  newCroppedLayer.resize(PCT * 100, PCT * 100, AnchorPosition.MIDDLECENTER);
-  newCroppedLayerWidth = newCroppedLayer.bounds[2] - newCroppedLayer.bounds[0];
-
-  // Get new width and height after resize
-  newCroppedLayerWidth = newCroppedLayer.bounds[2] - newCroppedLayer.bounds[0];
-  newCroppedLayerHeight = newCroppedLayer.bounds[3] - newCroppedLayer.bounds[1];
-  newCroppedLayer.translate(-newCroppedLayer.bounds[0], -newCroppedLayer.bounds[1]);
+  // cropAndResizeMockLayer(newCroppedLayer, newCroppedLayerWidth, newCroppedLayerHeight);
+  // newCroppedLayer.translate(-newCroppedLayer.bounds[0], -newCroppedLayer.bounds[1]);
   // -----------------
+
+
 
   /*
   * Loop through all the shirt colors here.
   *
   */
+
+  for (var mockCount = 0; mockCount < allMockupsGroup.layers.length; mockCount++) {
+    var activeMockLayer = allMockupsGroup.layers[mockCount];
+    doc.activeLayer = activeMockLayer;
+    activeMockLayer.visible = true;
+    doc.selection.select([
+      [cropBounds[0], cropBounds[1]],
+      [cropBounds[2], cropBounds[1]],
+      [cropBounds[2], cropBounds[3]],
+      [cropBounds[0], cropBounds[3]],
+    ])
+
+    doc.selection.copy();
+    var newCroppedLayer = resizedGroup.artLayers.add();
+    newCroppedLayer.name = activeMockLayer.name + "- cropped";
+    doc.paste();
+    doc.selection.deselect();
+    var newCroppedLayerBounds = newCroppedLayer.bounds;
+    activeMockLayer.visible = false;
+
+    cropAndResizeMockLayer(newCroppedLayer, newCroppedLayerWidth, newCroppedLayerHeight);
+    newCroppedLayer.translate(-newCroppedLayer.bounds[0], -newCroppedLayer.bounds[1]);
+  }
 
 
   // Mock image crop and resize successful!
@@ -100,10 +115,6 @@ try {
 } catch(err) {
   alert("error in testing crop process: " + err)
 }
-
-
-
-
 
 
 // for (var mockCounter = 0; mockCounter < allMockupsGroups.layers.length; mockCounter++) {
@@ -120,8 +131,10 @@ try {
 // }
 
 
-function cropMockShirtLayer() {
-
+function cropAndResizeMockLayer(layer, width, height) {
+  layer.resize(PCT * 100, PCT * 100, AnchorPosition.MIDDLECENTER);
+  width = layer.bounds[2] - layer.bounds[0];
+  height = layer.bounds[3] - layer.bounds[1];
 }
 
 function resizeMockShirtLayer() {
